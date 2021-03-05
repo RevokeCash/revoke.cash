309c405ed7f03a32cf09b25400f93a2345d110e8
Dawson Botsford <dawsonbotsford@gmail.com>
2021-03-05 08:41:12 -0700

    Move dapp-contract-list to public folder

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,9 @@
-export const TRUSTWALLET_BASE_URL = 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains'
-export const DAPP_LIST_BASE_URL = 'https://raw.githubusercontent.com/rkalis/revoke.cash/master/dapp-contract-list'
+export const TRUSTWALLET_BASE_URL =
+  "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains";
+export const DAPP_LIST_BASE_URL = "/dapp-contract-list";
 
-export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000000000000000000000000000'
-export const ERC20_BADGE_ADDRESS = '0xCb4Aae35333193232421E86Cd2E9b6C91f3B125F'
-export const T2CR_ADDRESS = '0xEbcf3bcA271B26ae4B162Ba560e243055Af0E679'
-export const TOKENS_VIEW_ADDRESS = '0xf9b9b5440340123B21BFf1dDAFE1ad6Feb9D6E7F'
+export const ADDRESS_ZERO =
+  "0x0000000000000000000000000000000000000000000000000000000000000000";
+export const ERC20_BADGE_ADDRESS = "0xCb4Aae35333193232421E86Cd2E9b6C91f3B125F";
+export const T2CR_ADDRESS = "0xEbcf3bcA271B26ae4B162Ba560e243055Af0E679";
+export const TOKENS_VIEW_ADDRESS = "0xf9b9b5440340123B21BFf1dDAFE1ad6Feb9D6E7F";
