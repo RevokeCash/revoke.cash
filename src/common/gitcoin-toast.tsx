da91a2fcf0f462b00aa0c6ebd193f05428419cda
Rosco Kalis <roscokalis@gmail.com>
2021-12-20 20:37:32 +0100

    Disable gitcoin toast

--- src/common/gitcoin-toast.tsx
+++ src/common/gitcoin-toast.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { toast } from 'react-toastify';
+// import React from 'react';
+// import { toast } from 'react-toastify';
 
 export const displayGitcoinToast = () => {
-  const toastContent = (
-    <div>
-      <div>
-        Hello friend! <span role="img" aria-label="wave">👋</span>
-      </div>
+  // const toastContent = (
+  //   <div>
+  //     <div>
+  //       Hello friend! <span role="img" aria-label="wave">👋</span>
+  //     </div>
 
-      <div>
-      Gitcoin Grants Round 12 is now live with over $2M in donation matching!
-      Please consider supporting Revoke.cash by donating to the <a href="https://gitcoin.co/grants/259/rosco-kalis-crypto-software-engineer" target="_blank" rel="noopener noreferrer" style={{ color: 'pink' }}>Revoke.cash grant</a>
-      </div>
-    </div>
-  )
+  //     <div>
+  //     Gitcoin Grants Round 12 is now live with over $2M in donation matching!
+  //     Please consider supporting Revoke.cash by donating to the <a href="https://gitcoin.co/grants/259/rosco-kalis-crypto-software-engineer" target="_blank" rel="noopener noreferrer" style={{ color: 'pink' }}>Revoke.cash grant</a>
+  //     </div>
+  //   </div>
+  // )
 
-  toast.error(toastContent, {
-    position: "top-left",
-    autoClose: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-  })
+  // toast.error(toastContent, {
+  //   position: "top-left",
+  //   autoClose: false,
+  //   closeOnClick: true,
+  //   pauseOnHover: true,
+  //   draggable: true,
+  //   progress: undefined,
+  // })
 }
