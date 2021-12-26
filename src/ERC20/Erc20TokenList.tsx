7236351d6db56cec33078e9d95f194aff4b535e6
Rosco Kalis <roscokalis@gmail.com>
2021-12-26 21:01:25 +0100

    Convert Erc20TokenList to functional components

--- src/ERC20/Erc20TokenList.tsx
+++ src/ERC20/Erc20TokenList.tsx
@@ -1,6 +1,6 @@
 import { Signer, Contract, providers } from 'ethers'
 import { Interface, getAddress, hexZeroPad } from 'ethers/lib/utils'
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import ClipLoader from 'react-spinners/ClipLoader'
 import { Erc20TokenData, TokenMapping } from '../common/interfaces'
 import Erc20Token from './Erc20Token'
@@ -8,9 +8,9 @@ import { isRegistered, getTokenIcon, toFloat } from '../common/util'
 import { getTokenData } from './util'
 import { ERC20 } from '../common/abis'
 
-type Props = {
+interface Props {
   provider: providers.Provider
-  chainId: number,
+  chainId: number
   filterRegisteredTokens: boolean
   filterZeroBalances: boolean
   tokenMapping?: TokenMapping
@@ -19,49 +19,43 @@ type Props = {
   inputAddress?: string
 }
 
-type State = {
-  tokens: Erc20TokenData[]
-  loading: boolean
-}
+function Erc20TokenList({
+  provider,
+  chainId,
+  filterRegisteredTokens,
+  filterZeroBalances,
+  tokenMapping,
+  signer,
+  signerAddress,
+  inputAddress
+}: Props) {
+  const [tokens, setTokens] = useState<Erc20TokenData[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
 
-class Erc20TokenList extends Component<Props, State> {
-  state: State = {
-    tokens: [],
-    loading: true,
-  }
+  useEffect(() => {
+    loadData()
+  }, [inputAddress])
 
-  componentDidMount() {
-    this.loadData()
-  }
+  const loadData = async () => {
+    if (!inputAddress) return
 
-  componentDidUpdate(prevProps: Props) {
-    if (this.props.inputAddress === prevProps.inputAddress) return
-    this.loadData()
-  }
-
-  async loadData() {
-    if (!this.props.inputAddress) return
-
-    // Reset existing state after update
-    this.setState({ tokens: [], loading: true })
+    setLoading(true)
 
     const erc20Interface = new Interface(ERC20)
-    const signerOrProvider = this.props.signer || this.props.provider
-
-    const latestBlockNumber = await this.props.provider.getBlockNumber();
+    const latestBlockNumber = await provider.getBlockNumber()
 
     // Get all approvals made from the input address
-    const approvals = await this.props.provider.getLogs({
+    const approvals = await provider.getLogs({
       fromBlock: 0,
       toBlock: latestBlockNumber,
-      topics: [erc20Interface.getEventTopic('Approval'), hexZeroPad(this.props.inputAddress, 32)]
+      topics: [erc20Interface.getEventTopic('Approval'), hexZeroPad(inputAddress, 32)]
     })
 
     // Get all transfers sent to the input address
-    const transfers = await this.props.provider.getLogs({
+    const transfers = await provider.getLogs({
       fromBlock: 0,
       toBlock: latestBlockNumber,
-      topics: [erc20Interface.getEventTopic('Transfer'), undefined, hexZeroPad(this.props.inputAddress, 32)]
+      topics: [erc20Interface.getEventTopic('Transfer'), undefined, hexZeroPad(inputAddress, 32)]
     })
 
     const allEvents = [...approvals, ...transfers];
@@ -69,24 +63,18 @@ class Erc20TokenList extends Component<Props, State> {
     // Filter unique token contract addresses and convert all events to Contract instances
     const tokenContracts = allEvents
       .filter((event, i) => i === allEvents.findIndex((other) => event.address === other.address))
-      .map((event) => new Contract(getAddress(event.address), ERC20, signerOrProvider))
-
-    // Return early if no tokens are found
-    if (tokenContracts.length === 0) {
-      this.setState({ loading: false })
-      return
-    }
+      .map((event) => new Contract(getAddress(event.address), ERC20, signer ?? provider))
 
     // Look up token data for all tokens, add their list of approvals,
     // and check if the token is registered in Kleros T2CR
     const unsortedTokens = await Promise.all(
       tokenContracts.map(async (contract) => {
         const tokenApprovals = approvals.filter(approval => approval.address === contract.address)
-        const registered = await isRegistered(contract.address, this.props.provider, this.props.tokenMapping)
-        const icon = await getTokenIcon(contract.address, this.props.chainId, this.props.tokenMapping)
+        const registered = await isRegistered(contract.address, provider, tokenMapping)
+        const icon = await getTokenIcon(contract.address, chainId, tokenMapping)
 
         try {
-          const tokenData = await getTokenData(contract, this.props.inputAddress, this.props.tokenMapping)
+          const tokenData = await getTokenData(contract, inputAddress, tokenMapping)
           return { ...tokenData, icon, contract, registered, approvals: tokenApprovals }
         } catch {
           // If the call to getTokenData() fails, the token is not an ERC20 token so
@@ -97,38 +85,37 @@ class Erc20TokenList extends Component<Props, State> {
     )
 
     // Filter undefined tokens and sort tokens alphabetically on token symbol
-    const tokens = unsortedTokens
+    const sortedTokens = unsortedTokens
       .filter((token) => token !== undefined)
       .sort((a: any, b: any) => a.symbol.localeCompare(b.symbol))
 
-    this.setState({ tokens, loading: false })
+    setTokens(sortedTokens)
+    setLoading(false)
   }
 
-  render() {
-    if (this.state.loading) {
-      return (<ClipLoader css="margin-bottom: 10px;" size={40} color={'#000'} loading={this.state.loading} />)
-    }
-
-    if (this.state.tokens.length === 0) {
-      return (<div className="TokenList">No token balances</div>)
-    }
-
-    const tokenComponents = this.state.tokens
-      .filter((token) => !this.props.filterRegisteredTokens || token.registered)
-      .filter((token) => !this.props.filterZeroBalances || !(toFloat(Number(token.balance), token.decimals) === '0.000'))
-      .map((token) => (
-        <Erc20Token
-          key={token.contract.address}
-          token={token}
-          provider={this.props.provider}
-          chainId={this.props.chainId}
-          signerAddress={this.props.signerAddress}
-          inputAddress={this.props.inputAddress}
-        />
-      ))
-
-    return (<div className="TokenList">{tokenComponents}</div>)
+  if (loading) {
+    return (<ClipLoader css="margin-bottom: 10px;" size={40} color={'#000'} loading={loading} />)
   }
+
+  if (tokens.length === 0) {
+    return (<div className="TokenList">No token balances</div>)
+  }
+
+  const tokenComponents = tokens
+  .filter((token) => !filterRegisteredTokens || token.registered)
+  .filter((token) => !filterZeroBalances || !(toFloat(Number(token.balance), token.decimals) === '0.000'))
+  .map((token) => (
+    <Erc20Token
+      key={token.contract.address}
+      token={token}
+      provider={provider}
+      chainId={chainId}
+      signerAddress={signerAddress}
+      inputAddress={inputAddress}
+    />
+  ))
+
+return (<div className="TokenList">{tokenComponents}</div>)
 }
 
 export default Erc20TokenList
