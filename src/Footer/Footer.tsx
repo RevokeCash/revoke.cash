046623255311bd0923baf350805cac2ee64880ef
Rosco Kalis <roscokalis@gmail.com>
2022-02-21 00:24:25 +0100

    Add Twitter to footer

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -3,7 +3,12 @@ import { ToastContainer } from "react-toastify";
 
 const Footer: React.FC = () => (
   <div style={{ padding: '20px' }}>
-    <p>Site created by <a href="https://twitter.com/RoscoKalis">Rosco Kalis</a> (<a href="https://github.com/rkalis/revoke.cash">Source Code</a>)</p>
+    <p>
+      Site created by{' '}
+      <a href="https://twitter.com/RoscoKalis">Rosco Kalis</a> / {' '}
+      <a href="https://github.com/rkalis/revoke.cash">Source Code</a> / {' '}
+      <a href="https://twitter.com/RevokeCash">Official Twitter</a>
+    </p>
     <p>Learn more: <a href="https://kalis.me/unlimited-erc20-allowances/">Unlimited ERC20 allowances considered harmful</a></p>
     <ToastContainer
       position="top-right"
