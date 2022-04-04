e83119272309157c1e5194cbc0950e8598803856
daws.eth <dawsonbotsford@gmail.com>
2022-04-04 08:51:48 -0700

    Use preact instead of react

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // preact support
+  // https://darrenwhite.dev/blog/nextjs-replace-react-with-preact
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
