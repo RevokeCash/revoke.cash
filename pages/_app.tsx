f1d958e378cb3543d45f2bc8b73c140c4abc3abc
Rosco Kalis <roscokalis@gmail.com>
2022-07-10 22:15:47 +0200

    Extract header and footer into app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
+import Footer from 'components/Footer/Footer';
+import Header from 'components/Header/Header';
 import { NextSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
+import { Container } from 'react-bootstrap';
+import { EthereumProvider } from 'utils/hooks/useEthereum';
 import '../styles/index.scss';
 
 const SafeHydrate = ({ children }) => {
@@ -41,7 +45,13 @@ const App = ({ Component, pageProps }: AppProps) => (
       ]}
     />
     <SafeHydrate>
-      <Component {...pageProps} />
+      <EthereumProvider>
+        <Container fluid className="App">
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </Container>
+      </EthereumProvider>
     </SafeHydrate>
     <Script>{`window.sa_event=window.sa_event||function(){var a=[].slice.call(arguments);window.sa_event.q?window.sa_event.q.push(a):window.sa_event.q=[a]};`}</Script>
     <Script async defer src="https://scripts.simpleanalyticscdn.com/latest.js" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,15 @@
 import { displayGitcoinToast } from 'components/common/gitcoin-toast';
 import Dashboard from 'components/Dashboard/Dashboard';
-import Footer from 'components/Footer/Footer';
-import Header from 'components/Header/Header';
 import { NextPage } from 'next';
 import React, { useEffect } from 'react';
-import { Container } from 'react-bootstrap';
 import 'react-toastify/dist/ReactToastify.css';
-import { EthereumProvider } from 'utils/hooks/useEthereum';
 
 const App: NextPage = () => {
   useEffect(() => {
     displayGitcoinToast();
   }, []);
 
-  return (
-    <EthereumProvider>
-      <Container fluid className="App">
-        <Header />
-        <Dashboard />
-        <Footer />
-      </Container>
-    </EthereumProvider>
-  );
+  return <Dashboard />;
 };
 
 export default App;
