2810e1e563c79d9dbe4c1489dde90c758c922eaa
Rosco Kalis <roscokalis@gmail.com>
2022-07-29 16:13:02 +0200

    Add small banner about extension (to be deleted in a week)

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,49 +7,67 @@ import NavLink from './NavLink';
 
 const Header: React.FC = () => {
   return (
-    <div
-      className="Header"
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'start',
-        gap: '10px',
-        marginTop: '10px',
-        marginBottom: '10px',
-      }}
-    >
+    <>
       <div
+        style={{
+          background: 'black',
+          color: 'white',
+          paddingTop: '10px',
+          paddingBottom: '10px',
+          width: '100vw',
+          left: 0,
+          top: 0,
+          right: 0,
+          position: 'absolute',
+        }}
+      >
+        Need extra security? Try the new Revoke.cash browser extension.
+      </div>
+      <div style={{ background: 'black', color: 'white', padding: '10px', margin: '10px' }} />
+      <div
+        className="Header"
         style={{
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'start',
-          gap: '2px',
+          gap: '10px',
+          marginTop: '10px',
+          marginBottom: '10px',
         }}
       >
-        <Row>
-          <Col className="my-auto">
-            <Link href="/">
-              <a>
-                <img className="logo" src="/revoke.svg" alt="Revoke.cash logo" />
-              </a>
-            </Link>
-          </Col>
-        </Row>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'start',
+            gap: '2px',
+          }}
+        >
+          <Row>
+            <Col className="my-auto">
+              <Link href="/">
+                <a>
+                  <img className="logo" src="/revoke.svg" alt="Revoke.cash logo" />
+                </a>
+              </Link>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="my-auto" style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
+              <NavLink to="/about" text="About" matchToHighlight="about" />
+              <NavLink to="/faq" text="FAQ" matchToHighlight="faq" />
+              <NavLink to="/extension" text="Extension" matchToHighlight="extension" />
+            </Col>
+          </Row>
+        </div>
         <Row>
           <Col className="my-auto" style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
-            <NavLink to="/about" text="About" matchToHighlight="about" />
-            <NavLink to="/faq" text="FAQ" matchToHighlight="faq" />
-            <NavLink to="/extension" text="Extension" matchToHighlight="extension" />
+            <DonateButton />
+            <ConnectButton />
           </Col>
         </Row>
       </div>
-      <Row>
-        <Col className="my-auto" style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
-          <DonateButton />
-          <ConnectButton />
-        </Col>
-      </Row>
-    </div>
+    </>
   );
 };
 
