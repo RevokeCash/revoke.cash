a89622ba5bc1aafaf61db672dc9d8cf13f8636f3
Rosco Kalis <roscokalis@gmail.com>
2022-08-12 13:09:10 +0200

    Add boilerplate Privacy Policy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const withPreact = require('next-plugin-preact');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/privacy-policy',
+        destination: '/privacy-policy.html',
+      },
+    ];
+  },
 };
 
 module.exports = withPreact(nextConfig);
