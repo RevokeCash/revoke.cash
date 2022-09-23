755849c9d5e749d8e3e8a00a647741e94447db81
Rosco Kalis <roscokalis@gmail.com>
2022-09-23 11:33:45 +0200

    Remove Gitcoin toast

--- components/common/gitcoin-toast.tsx
+++ components/common/gitcoin-toast.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { toast } from 'react-toastify';
+// import React from 'react';
+// import { toast } from 'react-toastify';
 
 export const displayGitcoinToast = () => {
-  const toastContent = (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-      <div>
-        Gitcoin Grants Round 15 is now live with over $2M in donation matching! Please consider supporting Revoke.cash
-        by donating to the{' '}
-        <a
-          href="https://gitcoin.co/grants/259/revokecash-helping-you-stay-safe-in-web3"
-          target="_blank"
-          style={{ color: 'black' }}
-        >
-          Revoke.cash grant
-        </a>
-      </div>
-    </div>
-  );
-  toast.info(toastContent, {
-    position: 'top-left',
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-  });
+  // const toastContent = (
+  //   <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+  //     <div>
+  //       Gitcoin Grants Round 15 is now live with over $2M in donation matching! Please consider supporting Revoke.cash
+  //       by donating to the{' '}
+  //       <a
+  //         href="https://gitcoin.co/grants/259/revokecash-helping-you-stay-safe-in-web3"
+  //         target="_blank"
+  //         style={{ color: 'black' }}
+  //       >
+  //         Revoke.cash grant
+  //       </a>
+  //     </div>
+  //   </div>
+  // );
+  // toast.info(toastContent, {
+  //   position: 'top-left',
+  //   closeOnClick: true,
+  //   pauseOnHover: true,
+  //   draggable: true,
+  // });
 };
