38cf8f49a826fbf4de89bc4d0852f675b9534360
Rosco Kalis <roscokalis@gmail.com>
2022-10-24 03:01:29 +0200

    Fix missing translation

--- components/Header/ChainSelectDropdown.tsx
+++ components/Header/ChainSelectDropdown.tsx
@@ -1,11 +1,13 @@
 import ChainLogo from 'components/common/ChainLogo';
 import { CHAIN_SELECT_MAINNETS, CHAIN_SELECT_TESTNETS } from 'lib/constants';
 import { useEthereum } from 'lib/hooks/useEthereum';
+import useTranslation from 'next-translate/useTranslation';
 import React from 'react';
 import { Dropdown } from 'react-bootstrap';
 import ChainSelectDropdownButton from './ChainSelectDropdownButton';
 
 const ChainSelectDropdown: React.FC = () => {
+  const { t } = useTranslation();
   const { selectedChainId } = useEthereum();
 
   return (
@@ -17,11 +19,11 @@ const ChainSelectDropdown: React.FC = () => {
         <ChainLogo chainId={selectedChainId} />
       </Dropdown.Toggle>
       <Dropdown.Menu style={{ height: '300px', width: '220px', overflowY: 'scroll' }}>
-        <Dropdown.Header>Mainnets</Dropdown.Header>
+        <Dropdown.Header>{t('dashboard:mainnets')}</Dropdown.Header>
         {CHAIN_SELECT_MAINNETS.map((chainId) => (
           <ChainSelectDropdownButton chainId={chainId} />
         ))}
-        <Dropdown.Header>Testnets</Dropdown.Header>
+        <Dropdown.Header>{t('dashboard:testnets')}</Dropdown.Header>
         {CHAIN_SELECT_TESTNETS.map((chainId) => (
           <ChainSelectDropdownButton chainId={chainId} />
         ))}
