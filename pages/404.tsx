9d93e1b401c730887cbba75b2a4e3d46dc959500
Rosco Kalis <roscokalis@gmail.com>
2022-10-24 02:38:05 +0200

    Add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,18 @@
+import Heading from 'components/common/Heading';
+import { defaultSEO } from 'lib/next-seo.config';
+import { NextPage } from 'next';
+import { NextSeo } from 'next-seo';
+import useTranslation from 'next-translate/useTranslation';
+
+const Error404: NextPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <NextSeo {...defaultSEO} title={t('common:meta.title')} description={t('common:meta.description')} />
+      <Heading text={t('common:errors.404.title')} type="h2" center />
+    </>
+  );
+};
+
+export default Error404;
