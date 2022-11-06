fd06937c234ab1251bff1d3697e9d05d62a89968
Rosco Kalis <roscokalis@gmail.com>
2022-11-06 12:47:56 +0100

    Remove TrustWallet stuff

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,7 +3,6 @@ import { ChainId } from 'eth-chains';
 import { providers } from 'ethers';
 import { IronSessionOptions } from 'iron-session';
 
-export const TRUSTWALLET_BASE_URL = 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains';
 export const DAPP_LIST_BASE_URL = '/dapp-contract-list';
 export const ETHEREUM_LISTS_CONTRACTS = 'https://raw.githubusercontent.com/ethereum-lists/contracts/main';
 export const CHROME_EXTENSION_URL =
