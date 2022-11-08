cdcecc5916fe7df7cbe4ed277006ccfd15b1b218
Rosco Kalis <roscokalis@gmail.com>
2022-11-08 01:12:05 +0100

    Add missed translations

--- components/Dashboard/TokenStandardSelection.tsx
+++ components/Dashboard/TokenStandardSelection.tsx
@@ -1,4 +1,5 @@
 import type { TokenStandard } from 'lib/interfaces';
+import useTranslation from 'next-translate/useTranslation';
 import Switch from 'react-switch';
 
 interface Props {
@@ -6,31 +7,34 @@ interface Props {
   setTokenStandard: (tokenStandard: TokenStandard) => void;
 }
 
-const TokenStandardSelection = ({ tokenStandard, setTokenStandard }: Props) => (
-  <div
-    style={{
-      marginBottom: '10px',
-      display: 'flex',
-      flexDirection: 'row',
-      gap: 10,
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-  >
-    <div>Tokens</div>
-    <div>
-      <Switch
-        checked={tokenStandard === 'ERC721'}
-        onChange={(checked: boolean) => setTokenStandard(checked ? 'ERC721' : 'ERC20')}
-        onColor="#000"
-        offColor="#000"
-        checkedIcon={false}
-        uncheckedIcon={false}
-        activeBoxShadow="0 0 2px 3px #aaa"
-      />
-    </div>
-    <div>NFTs</div>
-  </div>
-);
+const TokenStandardSelection = ({ tokenStandard, setTokenStandard }: Props) => {
+  const { t } = useTranslation();
 
+  return (
+    <div
+      style={{
+        marginBottom: '10px',
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div>{t('dashboard:controls.tokens')}</div>
+      <div>
+        <Switch
+          checked={tokenStandard === 'ERC721'}
+          onChange={(checked: boolean) => setTokenStandard(checked ? 'ERC721' : 'ERC20')}
+          onColor="#000"
+          offColor="#000"
+          checkedIcon={false}
+          uncheckedIcon={false}
+          activeBoxShadow="0 0 2px 3px #aaa"
+        />
+      </div>
+      <div>{t('dashboard:controls.nfts')}</div>
+    </div>
+  );
+};
 export default TokenStandardSelection;
