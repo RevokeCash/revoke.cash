7ca14f54e365ed06e57dc67079c915b829c8759e
Rosco Kalis <roscokalis@gmail.com>
2022-12-29 18:25:18 +0100

    Add demo video

--- components/landing/HeroSection.tsx
+++ components/landing/HeroSection.tsx
@@ -2,6 +2,7 @@ import Button from 'components/common/Button';
 import { useEthereum } from 'lib/hooks/useEthereum';
 import useTranslation from 'next-translate/useTranslation';
 import { useRouter } from 'next/router';
+import DemoVideo from './DemoVideo';
 import LandingParagraph from './LandingParagraph';
 import LandingSection from './LandingSection';
 
@@ -20,7 +21,7 @@ const HeroSection = () => {
   return (
     <LandingSection title={t('landing:hero_section.title')} size="lg">
       <LandingParagraph>{t('landing:hero_section.paragraph_1')}</LandingParagraph>
-      <div className="h-96 border border-black w-full max-w-5xl">TODO: add demo video here</div>
+      <DemoVideo />
       <Button style="primary" size="lg" className="font-bold mx-auto" onClick={connectAndRedirect}>
         {t('common:buttons.get_started')}
       </Button>
