00e7de9e26e11d3ab926d7010ee5ca545f276cfd
Rosco Kalis <roscokalis@gmail.com>
2022-12-29 19:59:44 +0100

    Reduce demo video quality

--- components/landing/DemoVideo.tsx
+++ components/landing/DemoVideo.tsx
@@ -2,7 +2,7 @@
 const DemoVideo = () => {
   return (
     <div className="border border-black w-full max-w-5xl">
-      <video autoPlay muted loop poster="/assets/images/demo-thumb.jpg">
+      <video controls muted loop preload="none" width="854" height="480" poster="/assets/images/demo-thumb.jpg">
         <source src="/assets/videos/demo.mp4" type="video/mp4" />
       </video>
     </div>
