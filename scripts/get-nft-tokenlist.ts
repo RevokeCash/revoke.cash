f61328e4bf27f5c5ef88a139cf57dfe2a883ec40
Rosco Kalis <roscokalis@gmail.com>
2023-01-02 17:04:42 +0100

    Small fixes and updates

--- scripts/get-nft-tokenlist.ts
+++ scripts/get-nft-tokenlist.ts
@@ -17,9 +17,9 @@ const symbolOverrides = {
   '0x49cF6f5d44E70224e2E23fDcdd2C053F30aDA28B': 'CLONE X',
   '0x1A92f7381B9F03921564a437210bB9396471050C': 'Cool Cats',
   '0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85': 'ENS Names',
-  '0xFaC7BEA255a6990f749363002136aF6556b31e04': 'ENS Names (old)',
 };
 
+// Lots of shared collections have the same address, so we need to override the symbol and logo
 const fullOverrides = {
   '0x059EDD72Cd353dF5106D2B9cC5ab83a52287aC3a': {
     symbol: 'Art Blocks',
@@ -37,6 +37,29 @@ const fullOverrides = {
   '0x942BC2d3e7a589FE5bd4A5C6eF9727DFd82F5C8a': {
     symbol: 'Art Blocks Explorations',
     logoURI: 'https://storage.googleapis.com/subgraph-images/1644278294885Squig.blk.circle.800px.png',
+  },
+  '0x495f947276749Ce646f68AC8c248420045cb7b5e': {
+    symbol: 'OpenSea Shared Storefront',
+    logoURI: '/assets/images/vendor/opensea.svg',
+  },
+  '0x57a204AA1042f6E66DD7730813f4024114d74f37': {
+    symbol: 'CyberKongz',
+    logoURI:
+      'https://i.seadn.io/gae/LIpf9z6Ux8uxn69auBME9FCTXpXqSYFo8ZLO1GaM8T7S3hiKScHaClXe0ZdhTv5br6FE2g5i-J5SobhKFsYfe6CIMCv-UfnrlYFWOM4?w=32&auto=format',
+  },
+  '0xdFDE78d2baEc499fe18f2bE74B6c287eED9511d7': {
+    symbol: 'BrainDrops',
+    logoURI:
+      'https://i.seadn.io/gae/U7CaccqE3lrtwrT9uZNrEtrnmwwBtX5ytA4lKPWKv26_rJ4hxgt3SDQyjG1Clz2ORAMK3LWeMWhfTbVEQYw_YerRGcNd2ogqvq7ajA?w=32&auto=format',
+  },
+  '0xd8B7cC75E22031a72D7b8393113eF2536e17bde6': {
+    symbol: 'GEN.ART Drop Collection',
+    logoURI:
+      'https://i.seadn.io/gae/DT84XA-mnt8Tnm6v61zC22KPdVuM1tSdE98MqGL97aJZH_UuPNF_SFAsb2fYJ1TDhwnr7KaVuIdIqrQgXYaxY1r9NPHU9QHJWMoE?w=32&auto=format',
+  },
+  '0xb7eC7bbd2d2193B47027247FC666fB342D23c4B5': {
+    symbol: 'Mirage Gallery Curated',
+    logoURI: 'https://i.seadn.io/gcs/files/1d15163410ad93eee7a5926af0f3968b.jpg?w=32&auto=format',
   },
 };
 
@@ -62,11 +85,6 @@ const updateNftTokenlist = async () => {
       if (currentVolume < 100 || !image || !primaryContract || !name) return undefined;
 
       const address = getAddress(primaryContract);
-
-      if (fullOverrides[address]) {
-        return [address, fullOverrides[address]];
-      }
-
       const symbol = symbolOverrides[address] ?? name;
 
       const nft = {
@@ -92,6 +110,11 @@ const updateNftTokenlist = async () => {
       shouldContinue = false;
     }
   }
+
+  // Add overrides
+  const originalMapping = JSON.parse(fs.readFileSync(NFT_TOKEN_MAPPING_PATH, 'utf8'));
+  const fullMapping = { '1': { ...originalMapping['1'], ...fullOverrides } };
+  fs.writeFileSync(NFT_TOKEN_MAPPING_PATH, JSON.stringify(fullMapping, null, 2));
 };
 
 updateNftTokenlist();
