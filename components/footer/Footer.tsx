515f73ead33d256cab53f6ed45889d1859b62ebf
Rosco Kalis <roscokalis@gmail.com>
2023-02-24 17:18:24 +0100

    Update Copyright notice for 2023

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
   return (
     <footer className="w-full flex flex-col md:flex-row gap-2 items-center justify-between py-8 px-4 md:py-4 md:px-8 bg-black dark:bg-zinc-800 text-zinc-300">
       <div className="flex flex-col md:flex-row gap-2 md:gap-4 items-center">
-        <div>© Revoke.cash 2022</div>
+        <div>© Revoke.cash 2023</div>
         <div className="flex gap-2 items-center">
           <Href href="/privacy-policy" underline="hover" router>
             {t('common:footer.privacy')}
