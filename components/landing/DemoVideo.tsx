bfbd6d4a289c31cbbaf727c959d8a3e6ae45367c
Rosco Kalis <roscokalis@gmail.com>
2023-03-05 22:33:42 +0100

    Fix layout shift due to video size

--- components/landing/DemoVideo.tsx
+++ components/landing/DemoVideo.tsx
@@ -1,7 +1,7 @@
 const DemoVideo = () => {
   return (
     <div className="border border-black w-full max-w-5xl">
-      <video controls muted loop preload="none" width="854" height="480" poster="/assets/images/demo-thumb.jpg">
+      <video className="aspect-[16/9] w-full" controls muted loop preload="none" poster="/assets/images/demo-thumb.jpg">
         <source src="/assets/videos/demo.mp4" type="video/mp4" />
       </video>
     </div>
