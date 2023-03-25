c56a60c3d72541cd1f5fba9b4a89bf37048a3516
Rosco Kalis <roscokalis@gmail.com>
2023-03-25 19:24:52 +0100

    Make explorer link depend on selected chain

--- components/address/AddressSocialShareButtons.tsx
+++ components/address/AddressSocialShareButtons.tsx
@@ -1,8 +1,10 @@
 import { ShareIcon } from '@heroicons/react/24/solid';
 import Button from 'components/common/Button';
 import LogoLink from 'components/common/LogoLink';
+import { useAddressPageContext } from 'lib/hooks/page-context/AddressPageContext';
 import { useColorTheme } from 'lib/hooks/useColorTheme';
 import { writeToClipBoard } from 'lib/utils';
+import { getChainExplorerUrl } from 'lib/utils/chains';
 import useTranslation from 'next-translate/useTranslation';
 
 interface Props {
@@ -12,14 +14,17 @@ interface Props {
 const AddressSocialShareButtons = ({ address }: Props) => {
   const { darkMode } = useColorTheme();
   const { t } = useTranslation();
+  const { selectedChainId } = useAddressPageContext();
+
+  const explorerUrl = getChainExplorerUrl(selectedChainId);
 
   return (
     <div className="flex items-center gap-2">
       <LogoLink src="/assets/images/vendor/opensea.svg" alt="OpenSea Link" href={`https://opensea.io/${address}`} />
       <LogoLink
         src={darkMode ? '/assets/images/vendor/etherscan-light.svg' : '/assets/images/vendor/etherscan.svg'}
-        alt="Etherscan Link"
-        href={`https://etherscan.io/address/${address}`}
+        alt="Block Explorer Link"
+        href={`${explorerUrl}/address/${address}`}
       />
       <Button style="tertiary" size="none" onClick={() => writeToClipBoard(location.href, t)}>
         <ShareIcon className="w-5 h-5" />
