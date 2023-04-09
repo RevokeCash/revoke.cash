e6385d20441ed4b1722330849421dbc4a85f0b86
Clonescody <clonescody@protonmail.com>
2023-04-09 13:18:47 -0700

    Add search input to table header for spenders

--- components/allowances/table/header/AllowanceTableHeader.tsx
+++ components/allowances/table/header/AllowanceTableHeader.tsx
@@ -1,17 +1,20 @@
 import { Table } from '@tanstack/react-table';
 import type { AllowanceData } from 'lib/interfaces';
+import AllowanceSearch from './AllowanceSearch';
 import FilterSelect from './FilterSelect';
 import SortSelect from './SortSelect';
 
 interface Props {
   table: Table<AllowanceData>;
+  filterByContract: (contract: string | null) => void;
 }
 
-const AllowanceTableHeader = ({ table }: Props) => {
+const AllowanceTableHeader = ({ table, filterByContract }: Props) => {
   return (
     <div className="flex justify-start gap-2 flex-col md:flex-row">
       <SortSelect table={table} />
       <FilterSelect table={table} />
+      <AllowanceSearch filterByContract={filterByContract} />
     </div>
   );
 };
