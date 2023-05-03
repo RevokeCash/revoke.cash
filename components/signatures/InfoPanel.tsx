89ccb684ebf8891102bc71fe4e2d5866c5127396
Rosco Kalis <roscokalis@gmail.com>
2023-05-03 21:33:13 +0200

    Add info panel about signatures

---
+++ components/signatures/InfoPanel.tsx
@@ -0,0 +1,18 @@
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
+import Trans from 'next-translate/Trans';
+import DashboardPanel from './DashboardPanel';
+
+const InfoPanel = () => {
+  return (
+    <DashboardPanel>
+      <div className="flex items-center gap-4">
+        <ExclamationTriangleIcon className="w-6 h-6 text-yellow-500 shrink-0" />
+        <div>
+          <Trans i18nKey="address:signatures.info.description" components={[<span className="italic" />]} />
+        </div>
+      </div>
+    </DashboardPanel>
+  );
+};
+
+export default InfoPanel;
--- components/signatures/SignaturesDashboard.tsx
+++ components/signatures/SignaturesDashboard.tsx
@@ -1,9 +1,11 @@
+import InfoPanel from './InfoPanel';
 import MarketplacePanel from './marketplace/MarketplacePanel';
 import PermitsPanel from './permit/PermitsPanel';
 
 const SignaturesDashboard = () => {
   return (
     <div className="flex flex-col gap-2">
+      <InfoPanel />
       <MarketplacePanel />
       <PermitsPanel />
     </div>
