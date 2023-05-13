47a36fa6f858073be052eeb19fee83e32473deda
Rosco Kalis <roscokalis@gmail.com>
2023-05-13 21:08:23 +0200

    Require acknowledgement of signature notice (#129)

--- components/signatures/InfoPanel.tsx
+++ components/signatures/InfoPanel.tsx
@@ -1,18 +1,29 @@
-import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
+import Button from 'components/common/Button';
+import { useAddressPageContext } from 'lib/hooks/page-context/AddressPageContext';
+import { useMounted } from 'lib/hooks/useMounted';
 import Trans from 'next-translate/Trans';
+import useTranslation from 'next-translate/useTranslation';
 import DashboardPanel from './DashboardPanel';
 
 const InfoPanel = () => {
+  const { t } = useTranslation();
+  const isMounted = useMounted();
+  const { signatureNoticeAcknowledged, acknowledgeSignatureNotice } = useAddressPageContext();
+
   return (
     <DashboardPanel>
-      <div className="flex items-center gap-4">
-        <ExclamationTriangleIcon className="w-6 h-6 text-yellow-500 shrink-0" />
+      <div className="flex flex-col sm:flex-row sm:items-center gap-2">
         <div>
           <Trans
             i18nKey="address:signatures.info.description"
             components={[<span className="italic" />, <span className="font-bold" />]}
           />
         </div>
+        {isMounted && !signatureNoticeAcknowledged && (
+          <Button size="md" style="primary" className="shrink-0" onClick={acknowledgeSignatureNotice}>
+            {t('common:buttons.understand')}
+          </Button>
+        )}
       </div>
     </DashboardPanel>
   );
