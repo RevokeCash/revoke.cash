0f53afe2c046614acf93e9c1cb1132657d55d891
Rosco Kalis <roscokalis@gmail.com>
2023-05-23 10:58:17 +0200

    Formatting fixes

--- layouts/PublicLayout.tsx
+++ layouts/PublicLayout.tsx
@@ -11,7 +11,7 @@ const PublicLayout = ({ children, searchBar = true }: Props) => {
   return (
     <div className="flex flex-col mx-auto min-h-screen gap-4">
       <Header searchBar={searchBar} />
-      <main className="max-w-7xl w-full mx-auto px-4 md:px-8">{children}</main>
+      <main className="max-w-7xl w-full mx-auto px-4 lg:px-8">{children}</main>
       <div className="flex flex-col justify-end grow">
         <Footer />
       </div>
