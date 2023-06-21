0e3b31fa38e8838488f0d712a677e7fe5dab78f4
Rosco Kalis <roscokalis@gmail.com>
2023-06-21 17:42:46 +0200

    Write article about revoking token approvals

--- components/common/MarkdownProse.tsx
+++ components/common/MarkdownProse.tsx
@@ -1,4 +1,7 @@
+import LiteYouTubeEmbed from 'react-lite-youtube-embed';
 import ReactMarkdown, { Components } from 'react-markdown';
+import { AllowElement } from 'react-markdown/lib/rehype-filter';
+import rehypeRaw from 'rehype-raw';
 import Href from './Href';
 import Prose from './Prose';
 
@@ -8,7 +11,7 @@ interface Props {
 }
 
 const MarkdownProse = ({ content, className }: Props) => {
-  const components: Components = {
+  const components: Components & Record<string, any> = {
     a: ({ href, children }) => {
       return (
         <Href href={href} underline="hover" external={!href.startsWith('/')} router={href.startsWith('/')} html>
@@ -16,11 +19,38 @@ const MarkdownProse = ({ content, className }: Props) => {
         </Href>
       );
     },
+    p: ({ children }) => {
+      return <p>{children}</p>;
+    },
+    // We create a custom component for YouTube videos because we want to use the lite-youtube-embed package
+    'youtube-video': (props: any) => {
+      return (
+        <div className="my-5">
+          <LiteYouTubeEmbed poster="maxresdefault" noCookie={true} {...props} />
+        </div>
+      );
+    },
+  };
+
+  const allowElement: AllowElement = ({ tagName, children }) => {
+    // Including YouTube videos in paragraphs causes hydration errors, so we "disallow" <p> tags that contain a
+    // <youtube-video> tag. Combined with unwrapDisallowed, this will remove the <p> tag and only render the video.
+    if (tagName === 'p' && children.some((child) => child.type === 'element' && child.tagName === 'youtube-video')) {
+      return false;
+    }
+
+    return true;
   };
 
   return (
     <Prose className={className}>
-      <ReactMarkdown children={content} components={components} />
+      <ReactMarkdown
+        children={content}
+        components={components}
+        rehypePlugins={[rehypeRaw]}
+        allowElement={allowElement}
+        unwrapDisallowed
+      />
     </Prose>
   );
 };
