7aa4e87aaf474dad96bfb0594615285536b21b79
Rosco Kalis <roscokalis@gmail.com>
2023-06-28 10:41:15 +0200

    Small style changes

--- components/address/AddressSocialShareButtons.tsx
+++ components/address/AddressSocialShareButtons.tsx
@@ -15,7 +15,7 @@ const AddressSocialShareButtons = ({ address }: Props) => {
   const explorerUrl = getChainExplorerUrl(selectedChainId);
 
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-6 sm:gap-2">
       <LogoLink src="/assets/images/vendor/opensea.svg" alt="OpenSea Link" href={`https://opensea.io/${address}`} />
       <LogoLink
         src={darkMode ? '/assets/images/vendor/etherscan-light.svg' : '/assets/images/vendor/etherscan.svg'}
