0aa6887096129526bc0b359d2ca8aa55c9f342f1
Rosco Kalis <roscokalis@gmail.com>
2023-07-07 10:24:37 +0200

    Add Trader Joe contracts to spender list

--- scripts/lint-checksums.ts
+++ scripts/lint-checksums.ts
@@ -2,25 +2,25 @@ import { utils } from 'ethers';
 import fs from 'fs/promises';
 import path from 'path';
 
-const dappListPath = path.join(__dirname, '..', 'public', 'data', 'spenders');
-
-const lintChecksums = async () => {
-  const chainIds = await fs.readdir(dappListPath);
+const lintChecksums = async (dataPath: string) => {
+  const chainIds = await fs.readdir(dataPath);
 
   await Promise.all(
     chainIds.map(async (chainId) => {
-      const chainPath = path.join(dappListPath, chainId);
+      const chainPath = path.join(dataPath, chainId);
       const files = await fs.readdir(chainPath);
 
       await Promise.all(
         files.map(async (file) => {
           const address = file.replace('.json', '');
           const checksummedAddress = utils.getAddress(address.toLowerCase());
-          fs.rename(path.join(chainPath, file), path.join(chainPath, `${checksummedAddress}.json`));
+
+          await fs.rename(path.join(chainPath, file), path.join(chainPath, `${checksummedAddress}.json`));
         })
       );
     })
   );
 };
 
-lintChecksums();
+lintChecksums(path.join(__dirname, '..', 'data', 'spenders'));
+lintChecksums(path.join(__dirname, '..', 'data', 'tokens'));
