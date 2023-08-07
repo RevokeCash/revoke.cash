fbd8ed54e9bb7d7e16f3cf19f15806681e4538cc
Rosco Kalis <roscokalis@gmail.com>
2023-08-07 10:53:38 +0100

    Add placeholder Learn pages for Permit / Permit2

--- lib/utils/markdown-content.ts
+++ lib/utils/markdown-content.ts
@@ -73,6 +73,8 @@ export const getSidebar = async (
         children: [
           getSidebarEntry('approvals/what-are-token-approvals', locale, directory, extended),
           getSidebarEntry('approvals/how-to-revoke-token-approvals', locale, directory, extended),
+          getSidebarEntry('approvals/what-are-permit-signatures', locale, directory, extended),
+          getSidebarEntry('approvals/what-is-permit2', locale, directory, extended),
         ],
       },
       {
