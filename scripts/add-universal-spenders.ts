9f9424a17b2ba061a90d7f6f68fe44bdfa239c38
Rosco Kalis <roscokalis@gmail.com>
2023-08-09 10:40:50 +0100

    Update labels

---
+++ scripts/add-universal-spenders.ts
@@ -0,0 +1,25 @@
+import { utils } from 'ethers';
+import fs from 'fs/promises';
+import { CHAIN_SELECT_MAINNETS } from 'lib/utils/chains';
+import path from 'path';
+
+// Universal Spenders shuold be added to every mainnet chain (e.g. Permit2)
+const UNIVERSAL_SPENDERS = {
+  '0x000000000022D473030F116dDEE9F6B43aC78BA3': {
+    name: 'Permit2',
+    label: 'Permit2',
+  },
+};
+
+const SPENDERS_BASE_PATH = path.join(__dirname, '..', 'data', 'spenders');
+
+CHAIN_SELECT_MAINNETS.forEach((chainId) => {
+  Object.entries(UNIVERSAL_SPENDERS).forEach(async ([address, spender]) => {
+    const chainPath = path.join(SPENDERS_BASE_PATH, `${chainId}`);
+    await fs.mkdir(chainPath, { recursive: true });
+
+    const spenderPath = path.join(chainPath, `${utils.getAddress(address)}.json`);
+    const spenderJson = JSON.stringify(spender, null, 2);
+    await fs.writeFile(spenderPath, spenderJson);
+  });
+});
