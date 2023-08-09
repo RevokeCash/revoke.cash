072622a77e8b375dfe4ff86de4dede98bab8d505
Rosco Kalis <roscokalis@gmail.com>
2023-08-09 10:43:49 +0100

    Fix toast close button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -52,8 +52,17 @@ const App = ({ Component, pageProps }: AppProps) => {
               className="text-center"
               toastClassName="border border-black bg-white text-zinc-900 dark:bg-black dark:border-white dark:text-zinc-100"
               progressClassName="bg-black dark:bg-white"
-              closeButton={() => (
-                <XMarkIcon className="w-6 h-6 text-zinc-500 hover:text-black dark:hover:text-white shrink-0" />
+              closeButton={({ closeToast, ariaLabel }) => (
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    closeToast(e);
+                  }}
+                  aria-label={ariaLabel}
+                  className='"w-6 h-6 text-zinc-500 hover:text-black dark:hover:text-white shrink-0'
+                >
+                  <XMarkIcon className="w-6 h-6" />
+                </button>
               )}
               position="top-right"
               icon={false}
