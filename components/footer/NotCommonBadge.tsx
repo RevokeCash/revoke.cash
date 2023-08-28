d4ff58b8f34cca1fc97f894afaf86e5c6271815e
Rosco Kalis <roscokalis@gmail.com>
2023-08-28 18:13:07 +0200

    Update NotCommon url again

--- components/footer/NotCommonBadge.tsx
+++ components/footer/NotCommonBadge.tsx
@@ -4,7 +4,7 @@ export default function NotCommonBadge() {
   const src =
     'https://dnskzpklqkqxj.cloudfront.net/assets/new_images/verified_badges/notcommon-badge-white-on-black-739ac63e120267f1af9ba769832d728fa8a648de1fcad3820e4b655b43afb6d7.svg';
   return (
-    <a href="https://not.co/revoke-cash?verify=true" target="_blank" referrerPolicy="origin">
+    <a href="https://notcommon.com/revokecash?verify=true" target="_blank" referrerPolicy="origin">
       <Image
         src={src}
         alt="NotCommon Verified"
