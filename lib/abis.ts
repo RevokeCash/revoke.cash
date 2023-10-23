0aabc38842279424f20c0ffeacbb8f69d0972670
Rosco Kalis <roscokalis@gmail.com>
2023-10-23 16:21:10 +0200

    Fix issue with USDC Permit signatures on Arbitrum

--- lib/abis.ts
+++ lib/abis.ts
@@ -16,6 +16,7 @@ export const ERC20_ABI = parseAbi([
   'function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external',
   'function nonces(address owner) public view returns (uint256)',
   'function DOMAIN_SEPARATOR() external view returns (bytes32)',
+  'function version() external view returns (string)', // This is not part of the standard but is used by some tokens
 ]);
 
 export const ERC721_ABI = parseAbi([
