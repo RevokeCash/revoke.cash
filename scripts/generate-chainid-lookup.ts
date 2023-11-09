a11c9cf610f7b9d0cdedc567dbab6d9d3e69b093
Rosco Kalis <roscokalis@gmail.com>
2023-11-09 12:33:31 +0100

    Small TS build fix

--- scripts/generate-chainid-lookup.ts
+++ scripts/generate-chainid-lookup.ts
@@ -1,10 +1,10 @@
-import { chains } from '@revoke.cash/chains';
+import { allChains } from '@revoke.cash/chains';
 import { writeFileSync } from 'fs';
 import { getChainName } from 'lib/utils/chains';
 import { join } from 'path';
 
 const path = join(__dirname, 'chainid-lookup.csv');
-const lookupCsvValues = Object.keys(chains.all())
+const lookupCsvValues = Object.keys(allChains())
   .map((chainId) => `${chainId},${getChainName(Number(chainId))}`)
   .join('\n');
 const lookupCsvHeader = 'chainId,chainName';
