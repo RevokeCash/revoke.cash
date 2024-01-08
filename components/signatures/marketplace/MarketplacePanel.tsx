1965a252ac31b105f4977404b1ec7d1b295074aa
Danil Aliforenko <siragled@gmail.com>
2024-01-08 03:03:24 +0100

    Filter marketplace addresses by approvals

--- components/signatures/marketplace/MarketplacePanel.tsx
+++ components/signatures/marketplace/MarketplacePanel.tsx
@@ -2,14 +2,52 @@ import { InformationCircleIcon } from '@heroicons/react/24/outline';
 import Card from 'components/common/Card';
 import WithHoverTooltip from 'components/common/WithHoverTooltip';
 import { useMarketplaces } from 'lib/hooks/ethereum/useMarketplaces';
-import { useAddressPageContext } from 'lib/hooks/page-context/AddressPageContext';
+import { useAddressAllowances, useAddressPageContext } from 'lib/hooks/page-context/AddressPageContext';
 import useTranslation from 'next-translate/useTranslation';
+import { useEffect, useLayoutEffect, useState } from 'react';
+import { getSpenderData } from '../../../lib/utils/whois';
+import Error from '../../common/Error';
+import TableBodyLoader from '../../common/TableBodyLoader';
 import MarketplaceEntry from './MarketplaceEntry';
 
 const MarketplacePanel = () => {
+  const ROW_HEIGHT = 52;
+  const [loaderHeight, setLoaderHeight] = useState<number>(ROW_HEIGHT * 12);
+
+  useLayoutEffect(() => {
+    // 530 is around the size of the headers and controls (and at least 2 row also on small screens)
+    setLoaderHeight(Math.max(window.innerHeight - 530, 2 * ROW_HEIGHT + 68));
+  }, []);
+
   const { t } = useTranslation();
   const { selectedChainId } = useAddressPageContext();
   const marketplaces = useMarketplaces(selectedChainId);
+  const { allowances, error: allowancesError, isLoading: isAllowancesLoading } = useAddressAllowances();
+  const [spenderNames, setSpenderNames] = useState<string[]>([]);
+  const [namesFetched, setNamesFetched] = useState(false); // New state variable
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const names = await Promise.all(
+          allowances.map(async (allowance) => {
+            try {
+              const spenderData = await getSpenderData(allowance.spender, selectedChainId);
+              return spenderData?.name || null;
+            } catch (error) {
+              console.error('Error fetching spender names:', error);
+              return null;
+            }
+          }),
+        );
+
+        setSpenderNames(names.filter((name) => name !== null) as string[]);
+        setNamesFetched(true); // New state variable
+      } catch (error) {
+        console.error('Error fetching spender names:', error);
+      }
+    })();
+  }, [allowances, selectedChainId]);
 
   const title = (
     <div className="flex items-center gap-2">
@@ -22,7 +60,29 @@ const MarketplacePanel = () => {
     </div>
   );
 
-  if (marketplaces.length === 0) {
+  if (allowancesError) {
+    return (
+      <Card title={title} className="w-full flex justify-center items-center h-12">
+        <Error error={allowancesError} />
+      </Card>
+    );
+  }
+
+  const isLoading = isAllowancesLoading || (!namesFetched && spenderNames.length === 0);
+  const filteredMarketplaces = marketplaces.filter((marketplace) => spenderNames.includes(marketplace.name));
+
+  if (isLoading) {
+    return (
+      <Card title={title} className="w-full p-0">
+        <table className="w-full border-collapse">
+          <TableBodyLoader columns={1} rows={Math.floor(loaderHeight / ROW_HEIGHT)} className="max-sm:hidden" />
+          <TableBodyLoader columns={1} rows={Math.floor((loaderHeight - 68) / ROW_HEIGHT)} className="sm:hidden" />
+        </table>
+      </Card>
+    );
+  }
+
+  if (filteredMarketplaces.length === 0) {
     return (
       <Card title={title} className="w-full flex justify-center items-center h-12">
         <p className="text-center">{t('address:signatures.marketplaces.none_found')}</p>
@@ -33,7 +93,7 @@ const MarketplacePanel = () => {
   return (
     <Card title={title} className="p-0 overflow-x-scroll whitespace-nowrap scrollbar-hide">
       <div className="w-full">
-        {marketplaces.map((marketplace) => (
+        {filteredMarketplaces.map((marketplace) => (
           <MarketplaceEntry key={marketplace.name} marketplace={marketplace} />
         ))}
       </div>
