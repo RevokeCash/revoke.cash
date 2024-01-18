b19e82c080db8a59d1e1a50e55d05bb27b578d89
sabal poudel <sabalenfiniy@gmail.com>
2024-01-18 10:38:58 +0545

    Issue:157 -- Refactored ChainSelect Component by creating smaller, modular components for improved code organization.

--- components/common/ChainSelect.tsx
+++ components/common/ChainSelect.tsx
@@ -1,16 +1,19 @@
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import ChainLogo from 'components/common/ChainLogo';
-import Select from 'components/common/Select';
-import { useColorTheme } from 'lib/hooks/useColorTheme';
-import { useMounted } from 'lib/hooks/useMounted';
-import { CHAIN_SELECT_MAINNETS, CHAIN_SELECT_TESTNETS, getChainName, isSupportedChain } from 'lib/utils/chains';
 import useTranslation from 'next-translate/useTranslation';
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
 import { OptionProps } from 'react-select';
 import { twMerge } from 'tailwind-merge';
+
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+
+import ChainLogo from 'components/common/ChainLogo';
+import Select from 'components/common/Select';
 import Button from './Button';
 import Chevron from './Chevron';
 import PlaceholderIcon from './PlaceholderIcon';
+
+import { useColorTheme } from 'lib/hooks/useColorTheme';
+import { useMounted } from 'lib/hooks/useMounted';
+import { CHAIN_SELECT_MAINNETS, CHAIN_SELECT_TESTNETS, getChainName, isSupportedChain } from 'lib/utils/chains';
 
 interface ChainOption {
   value: string;
@@ -20,19 +23,18 @@ interface ChainOption {
 interface Props {
   selected: number;
   chainIds?: number[];
-  onSelect?: (chainId: number) => void;
-  menuAlign?: 'left' | 'right';
   instanceId?: string;
-  showNames?: boolean;
+  menuAlign?: 'left' | 'right';
+  onSelect?: (chainId: number) => void;
+  // showNames?: boolean;
 }
 
-const ChainSelect = ({ onSelect, selected, menuAlign, chainIds, instanceId, showNames }: Props) => {
-  const isMounted = useMounted();
+const ChainSelect = ({ onSelect, selected, menuAlign, chainIds, instanceId }: Props) => {
   const { t } = useTranslation();
   const { darkMode } = useColorTheme();
 
-  // track react select open or not
-  const [isSelectOpen, setIsSelectOpen] = useState<boolean>(false);
+  // Track whether the React Select is open or not
+  const [isSelectOpen, setSelectOpen] = useState<boolean>(false);
 
   const mainnetOptions = (chainIds ?? CHAIN_SELECT_MAINNETS).map((chainId) => ({
     value: getChainName(chainId),
@@ -55,88 +57,105 @@ const ChainSelect = ({ onSelect, selected, menuAlign, chainIds, instanceId, show
     },
   ];
 
-  const closeSelect = () => {
-    setIsSelectOpen(false);
-  };
+  const handleSelectClose = () => setSelectOpen(false);
+
+  const toggleSelectClose = () => setSelectOpen((prev) => !prev);
 
   const onChange = ({ chainId }: ChainOption) => {
     onSelect && onSelect(chainId);
-    closeSelect();
-  };
-
-  //  Custom Option component for React Select to display Chain logo and name
-  const CustomOption = (props: OptionProps<ChainOption>) => {
-    const { data, innerProps, isDisabled, isSelected } = props;
-    return (
-      <div
-        className={twMerge(
-          isSelected ? 'bg-zinc-800' : '',
-          'flex items-center gap-1 p-2  hover:bg-zinc-800',
-          isDisabled ? 'cursor-not-allowed bg-zinc-300' : 'cursor-pointer',
-        )}
-        {...innerProps}
-      >
-        {isMounted ? (
-          <ChainLogo chainId={data.chainId} />
-        ) : (
-          <PlaceholderIcon size={24} border className="bg-transparent" />
-        )}
-        <div>{data.value}</div>
-      </div>
-    );
-  };
-
-  const CustomDropdownIndicator = () => {
-    return <MagnifyingGlassIcon className="w-5 h-5 dark:text-white" />;
+    handleSelectClose();
   };
 
   return (
-    <>
-      <div className="relative">
-        <Button
-          onClick={() => setIsSelectOpen((prev) => !prev)}
-          className="flex items-center pl-3 pr-2 px-2 h-9 font-normal rounded-lg focus-visible:outline-none focus-visible:ring-black focus-visible:dark:ring-white focus-visible:ring-1"
-        >
-          <>
-            <div className="flex items-center gap-1">
-              {isMounted ? (
-                <ChainLogo chainId={selected} />
-              ) : (
-                <PlaceholderIcon size={24} border className="bg-transparent" />
-              )}
-            </div>
-            <Chevron className="w-5 h-5 fill-black dark:fill-white" />
-          </>
-        </Button>
-        {isSelectOpen && <div className="fixed z-10 inset-0" onClick={closeSelect} />}
-        {isSelectOpen && (
-          <div className="absolute z-20 mt-2">
-            <Select
-              autoFocus
-              menuIsOpen={isSelectOpen}
-              instanceId={instanceId ?? 'chain-select'}
-              classNamePrefix="chain-select"
-              aria-label="Select Network"
-              size="md"
-              className="shrink-0"
-              controlTheme={darkMode ? 'dark' : 'light'}
-              menuTheme={darkMode ? 'dark' : 'light'}
-              value={groups.flatMap((group) => group.options).find((option) => option.chainId === selected)}
-              options={chainIds ? mainnetOptions : groups}
-              isOptionDisabled={(option) => !isSupportedChain(option.chainId)}
-              onChange={onChange}
-              components={{ Option: CustomOption, DropdownIndicator: CustomDropdownIndicator }}
-              menuPlacement="bottom"
-              minMenuWidth="14.5rem"
-              minControlWidth="14.5rem"
-              placeholder={<PlaceholderIcon size={24} border />}
-              menuAlign={menuAlign}
-            />
-          </div>
-        )}
-      </div>
-    </>
+    <SelectOverlay
+      isOpen={isSelectOpen}
+      onClose={handleSelectClose}
+      target={<TargetButton ontoggle={toggleSelectClose} chainId={selected} />}
+    >
+      <Select
+        size="md"
+        autoFocus
+        onChange={onChange}
+        className="shrink-0"
+        menuAlign={menuAlign}
+        menuPlacement="bottom"
+        minMenuWidth="14.5rem"
+        minControlWidth="14.5rem"
+        menuIsOpen={isSelectOpen}
+        aria-label="Select Network"
+        classNamePrefix="chain-select"
+        menuTheme={darkMode ? 'dark' : 'light'}
+        instanceId={instanceId ?? 'chain-select'}
+        controlTheme={darkMode ? 'dark' : 'light'}
+        options={chainIds ? mainnetOptions : groups}
+        placeholder={<PlaceholderIcon size={24} border />}
+        isOptionDisabled={(option) => !isSupportedChain(option.chainId)}
+        components={{ Option: CustomOption, DropdownIndicator: CustomDropdownIndicator }}
+        value={groups.flatMap((group) => group.options).find((option) => option.chainId === selected)}
+      />
+    </SelectOverlay>
   );
 };
 
 export default ChainSelect;
+
+// Component to display the selected Chain and toggle the Select
+const TargetButton = ({ ontoggle, chainId }: { ontoggle: () => void; chainId: number }) => (
+  <Button
+    onClick={ontoggle}
+    className="flex items-center pl-3 pr-2 px-2 h-9 font-normal rounded-lg focus-visible:outline-none focus-visible:ring-black focus-visible:dark:ring-white focus-visible:ring-1"
+  >
+    <div className="flex items-center gap-1">
+      <ChainLogoMounted chainId={chainId} />
+    </div>
+    <Chevron className="w-5 h-5 fill-black dark:fill-white" />
+  </Button>
+);
+
+// Suggestion: Is it better to place this logic in the main file, ChainLogo.tsx?
+const ChainLogoMounted = ({ chainId }: { chainId: number }) => {
+  const isMounted = useMounted();
+  if (isMounted) return <ChainLogo chainId={chainId} />;
+  else return <PlaceholderIcon size={24} border className="bg-transparent" />;
+};
+
+// Custom DropdownIndicator component for React Select to display a Search Icon
+const CustomDropdownIndicator = () => {
+  return <MagnifyingGlassIcon className="w-5 h-5 dark:text-white" />;
+};
+
+// Custom Option component for React Select to display Chain logo and name
+const CustomOption = (props: OptionProps<ChainOption>) => {
+  const { data, innerProps, isDisabled, isSelected } = props;
+  return (
+    <div
+      className={twMerge(
+        isSelected ? 'bg-zinc-800' : '',
+        'flex items-center gap-1 p-2  hover:bg-zinc-800',
+        isDisabled ? 'cursor-not-allowed bg-zinc-300' : 'cursor-pointer',
+      )}
+      {...innerProps}
+    >
+      <ChainLogoMounted chainId={data.chainId} />
+      <div>{data.value}</div>
+    </div>
+  );
+};
+
+interface SelectOverlayProps {
+  isOpen: boolean;
+  target: ReactNode;
+  children: ReactNode;
+  onClose: () => void;
+}
+
+// Overlay component to wrap React select to achieve text filtering on dropdown
+const SelectOverlay = ({ isOpen, target, children, onClose }: SelectOverlayProps) => {
+  return (
+    <div className="relative">
+      {target}
+      {isOpen && <div className="fixed z-10 inset-0" onClick={onClose} />}
+      {isOpen && <div className="absolute z-20 mt-2">{children}</div>}
+    </div>
+  );
+};
