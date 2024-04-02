5ad564ab29b1efcada26932361b49b26f752549c
Rosco Kalis <roscokalis@gmail.com>
2024-04-02 12:14:23 +0800

    Remove Sentry profiler + reduce sample rate

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -8,8 +8,7 @@ Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-  profilesSampleRate: 1.0,
+  tracesSampleRate: 0.1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
@@ -33,6 +32,5 @@ Sentry.init({
     Sentry.browserTracingIntegration({
       enableInp: true,
     }),
-    Sentry.browserProfilingIntegration(),
   ],
 });
