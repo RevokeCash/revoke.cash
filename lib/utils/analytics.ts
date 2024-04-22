0d2ebfa6b4bcccf6bbd99615a6a7cb55b196c57f
Rosco Kalis <roscokalis@gmail.com>
2024-04-22 18:38:34 +0800

    Only do analytics in a browser environment

--- lib/utils/analytics.ts
+++ lib/utils/analytics.ts
@@ -1,12 +1,16 @@
 import mixpanel from 'mixpanel-browser';
 
 export const init = () => {
+  if (typeof window === 'undefined') return;
+
   if (process.env.NEXT_PUBLIC_MIXPANEL_API_KEY) {
     mixpanel?.init(process.env.NEXT_PUBLIC_MIXPANEL_API_KEY, { ip: false });
   }
 };
 
 export const track = (eventName: string, eventProperties: any) => {
+  if (typeof window === 'undefined') return;
+
   if (process.env.NEXT_PUBLIC_MIXPANEL_API_KEY) {
     mixpanel?.track(eventName, eventProperties);
   }
