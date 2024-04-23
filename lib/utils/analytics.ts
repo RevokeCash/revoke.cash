eb97d5dd9f5c4cb446d52e2cb174c9dc6878f7d8
Rosco Kalis <roscokalis@gmail.com>
2024-04-24 00:09:14 +0800

    Remove remaining Mixpanel references

--- lib/utils/analytics.ts
+++ lib/utils/analytics.ts
@@ -1,15 +1,6 @@
-// import mixpanel from 'mixpanel';
-
-export const init = () => {
-  // if (process.env.NEXT_PUBLIC_MIXPANEL_API_KEY) {
-  //   mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_API_KEY, { geolocate: false });
-  // }
-};
+export const init = () => {};
 
 export const track = (eventName: string, eventProperties: any) => {
-  // if (process.env.NEXT_PUBLIC_MIXPANEL_API_KEY) {
-  //   mixpanel.track(eventName, eventProperties);
-  // }
   if (typeof window === 'undefined') return;
 
   (window as any)?.sa_event(eventName, eventProperties);
