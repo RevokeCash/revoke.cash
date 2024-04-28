5e233c5187dfb92bcc18922dd7077db79c679f51
Rosco Kalis <roscokalis@gmail.com>
2024-04-28 18:45:12 +0800

    Fix mobile menu open/close bug

--- components/header/MobileMenu.tsx
+++ components/header/MobileMenu.tsx
@@ -3,32 +3,29 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import Button from 'components/common/Button';
 import WalletIndicator from 'components/header/WalletIndicator';
 import useTranslation from 'next-translate/useTranslation';
-import { useRouter } from 'next/router';
-import { Fragment, useEffect, useRef, useState } from 'react';
+import { Fragment, useRef, useState } from 'react';
 import DonateButton from '../common/DonateButton';
 import NavLink from './NavLink';
 
 const MobileMenu = () => {
-  const router = useRouter();
   const { t } = useTranslation();
   const focusRef = useRef(null);
 
   const [open, setOpen] = useState(false);
-  const toggleOpen = () => setOpen(!open);
-
-  // Close menu on navigation
-  useEffect(() => {
-    setOpen(false);
-  }, [router.asPath]);
 
   return (
     <div className="flex flex-col">
-      <Button style="none" size="none" onClick={toggleOpen} aria-label="Toggle Menu">
-        {!open && <Bars3Icon className="h-8 w-8" />}
-        {open && <XMarkIcon className="h-8 w-8" />}
-      </Button>
+      {open ? (
+        <Button style="none" size="none" onClick={() => setOpen(false)} aria-label="Close Menu">
+          <XMarkIcon className="h-8 w-8" />
+        </Button>
+      ) : (
+        <Button style="none" size="none" onClick={() => setOpen(true)} aria-label="Open Menu">
+          <Bars3Icon className="h-8 w-8" />
+        </Button>
+      )}
       <Transition.Root show={open} as={Fragment}>
-        <Dialog as="div" className="absolute inset-0 top-[4.5rem]" initialFocus={focusRef} onClose={setOpen}>
+        <Dialog as="div" className="absolute inset-0" initialFocus={focusRef} onClose={setOpen}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -38,7 +35,7 @@ const MobileMenu = () => {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <Dialog.Panel className="z-10 overflow-y-auto bg-white dark:bg-black w-screen h-screen">
+            <Dialog.Panel className="absolute inset-0 top-[4.5rem] z-10 overflow-y-auto bg-white dark:bg-black w-screen h-screen">
               <div className="flex flex-col items-center gap-6 p-12">
                 <button ref={focusRef} /> {/* Focus trap */}
                 <WalletIndicator menuAlign="right" size="none" style="tertiary" className="text-lg" />
