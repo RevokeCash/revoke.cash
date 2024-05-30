9fe1b7f86b82636c2596d93a413f1a9000f2aabf
Rosco Kalis <roscokalis@gmail.com>
2024-05-30 19:24:27 +0200

    Add Jason Doyle's pentesting guide

--- components/common/MarkdownProse.tsx
+++ components/common/MarkdownProse.tsx
@@ -1,4 +1,7 @@
 import ReactMarkdown, { Components } from 'react-markdown';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import javascript from 'react-syntax-highlighter/dist/esm/languages/prism/javascript';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import remarkDirective from 'remark-directive';
 import remarkDirectiveRehype from 'remark-directive-rehype';
 import remarkGfm from 'remark-gfm';
@@ -6,6 +9,8 @@ import Divider from './Divider';
 import Href from './Href';
 import Prose from './Prose';
 import YouTubeEmbed from './YouTubeEmbed';
+
+SyntaxHighlighter.registerLanguage('javascript', javascript);
 
 interface Props {
   content: string;
@@ -22,12 +27,23 @@ const MarkdownProse = ({ content, className }: Props) => {
         </>
       );
     },
-    a: ({ href, children }) => {
+    a: ({ href, children, rel }) => {
       return (
-        <Href href={href} underline="hover" external={!href.startsWith('/')} router={href.startsWith('/')} html>
+        <Href
+          href={href}
+          rel={rel}
+          underline="hover"
+          external={!href.startsWith('/')}
+          router={href.startsWith('/')}
+          html
+        >
           {children}
         </Href>
       );
+    },
+    // Add a nofollow attribute to these links (can only take a link as child)
+    nofollow: (props: any) => {
+      return { ...props.children, props: { ...props.children.props, rel: 'nofollow' } };
     },
     p: ({ children }) => {
       return <p>{children}</p>;
@@ -35,6 +51,26 @@ const MarkdownProse = ({ content, className }: Props) => {
     // We create a custom component for YouTube videos because we want to use the lite-youtube-embed package
     'youtube-video': (props: any) => {
       return <YouTubeEmbed {...props} />;
+    },
+    // <pre> is handled by the code component (SyntaxHighlighter)
+    pre: ({ children }) => <>{children}</>,
+    code({ node, inline, className, children, ...props }: any) {
+      const language = /language-(\w+)/.exec(className || '')?.[1];
+
+      if (!language) {
+        return <code {...props}>{children}</code>;
+      }
+
+      return (
+        // @ts-ignore (Not sure why this is throwing an error)
+        <SyntaxHighlighter
+          customStyle={{ marginTop: '1.25em', marginBottom: '1.25em' }}
+          style={dracula}
+          language={language}
+          children={String(children).replace(/\n$/, '')}
+          {...props}
+        />
+      );
     },
   };
 
