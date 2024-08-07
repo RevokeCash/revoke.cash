c305f6520cea5bd0cee57c44cfb3ba9df5306aed
Rosco Kalis <roscokalis@gmail.com>
2024-08-07 18:04:08 +0200

    Extract RiskTolltip into its own component and some more risk refactoring

--- lib/whois/spender/risk/WebacySpenderRiskDataSource.ts
+++ lib/whois/spender/risk/WebacySpenderRiskDataSource.ts
@@ -31,38 +31,48 @@ export class WebacySpenderRiskDataSource implements SpenderDataSource {
       console.log(elapsedTime, 'Webacy', address);
 
       const BLOCKLIST_TAGS = ['blacklist_doubt', 'stealing_attack', 'phishing_activities', 'is_blacklisted'];
+      const UNSAFE_TAGS = ['can_take_back_ownership', 'transfer_without_approval', 'restricted_approval'];
+
       const BLOCKLIST_CATEGORIES = ['contract_reported', 'possible_drainer'];
       const UNSAFE_CATEGORIES = [
         'poor_developer_practices',
         'contract_brickable',
-        'governance_issues',
         'contract_issues',
         'financially_lopsided',
         'improper_signature_validation',
       ];
-      const IGNORE_CATEGORIES = ['miner_manipulable', 'address_characteristics', 'fraudulent_malicious']; // Note: We're ignoring fraudulent_malicious since it is too braod. Instead we check for specific tags
+
+      // Note: We're ignoring fraudulent_malicious since it is too braod. Instead we check for specific tags
+      const IGNORE_CATEGORIES = [
+        'governance_issues',
+        'miner_manipulable',
+        'address_characteristics',
+        'fraudulent_malicious',
+      ];
 
       const riskFactors: RiskFactor[] = (data?.issues ?? []).flatMap((issue) => {
         const tags = issue?.tags?.map((tag: any) => tag.key) as string[];
 
         const tagFactors = tags.flatMap((tag) => {
+          if (tag === 'is_closed_source') return [{ type: 'closed_source', source: 'webacy' }];
+          if (UNSAFE_TAGS.includes(tag)) return [{ type: 'unsafe', source: 'webacy' }];
           if (BLOCKLIST_TAGS.includes(tag)) return [{ type: 'blocklist', source: 'webacy' }];
           return [];
         });
 
         const categoryFactors = Object.keys(issue?.categories ?? {}).flatMap((category: string) => {
-          if (BLOCKLIST_CATEGORIES.includes(category)) return [{ type: 'blocklist', source: 'webacy' }];
-          if (UNSAFE_CATEGORIES.includes(category)) return [{ type: 'unsafe', source: 'webacy' }];
           if (IGNORE_CATEGORIES.includes(category)) return [];
+          if (UNSAFE_CATEGORIES.includes(category)) return [{ type: 'unsafe', source: 'webacy' }];
+          if (BLOCKLIST_CATEGORIES.includes(category)) return [{ type: 'blocklist', source: 'webacy' }];
           return [{ type: category, source: 'webacy' }];
         });
 
         return [...tagFactors, ...categoryFactors];
       });
 
-      if (data?.isContract === false) riskFactors.push({ type: 'is_eoa', source: 'webacy' });
+      if (data?.isContract === false) riskFactors.push({ type: 'eoa', source: 'webacy' });
 
-      return { riskFactors: deduplicateArray(riskFactors, (a, b) => a.type === b.type) };
+      return { riskFactors: deduplicateArray(riskFactors, (a, b) => a.type === b.type && a.data === b.data) };
     } catch {
       return null;
     }
