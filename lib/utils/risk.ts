5583124f0e569119af87515fdf7c6aff5ec5bf0b
Rosco Kalis <roscokalis@gmail.com>
2024-08-07 18:04:08 +0200

    Extract RiskTolltip into its own component and some more risk refactoring

--- lib/utils/risk.ts
+++ lib/utils/risk.ts
@@ -1,10 +1,11 @@
-import { RiskFactor } from 'lib/interfaces';
+import { RiskFactor, RiskLevel } from 'lib/interfaces';
 import { track } from './analytics';
 
 export const RiskFactorScore = {
   blocklist: 100,
-  is_eoa: 100,
+  closed_source: 50,
   deprecated: 100,
+  eoa: 100,
   exploit: 100,
   unsafe: 50,
 };
@@ -21,11 +22,20 @@ export const filterUnknownRiskFactors = (riskFactors: RiskFactor[]): RiskFactor[
 };
 
 export const calculateRiskScore = (riskFactors: RiskFactor[]): number => {
-  return Math.max(
-    Math.min(
-      riskFactors.reduce((acc, riskFactor) => acc + RiskFactorScore[riskFactor.type], 0),
-      100,
-    ),
-    0,
-  );
+  const riskScore = riskFactors.reduce((acc, riskFactor) => acc + (RiskFactorScore[riskFactor.type] ?? 0), 0);
+  return Math.max(Math.min(riskScore, 100), 0);
+};
+
+export const getRiskLevel = (riskFactors: RiskFactor[]): RiskLevel => {
+  if (riskFactors.length === 0) return 'unknown';
+
+  const riskScore = calculateRiskScore(riskFactors);
+
+  if (riskScore >= 80) return 'high';
+  if (riskScore <= 20) return 'low';
+
+  // If the risk score is not a clear high or low, we need at least multiple risk factors to consider it medium instead of unknown
+  if (riskFactors.length > 1) return 'medium';
+
+  return 'unknown';
 };
