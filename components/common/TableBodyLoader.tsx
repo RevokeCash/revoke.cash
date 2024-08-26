b7a2a2be7c664bfabcf667bb53d618f5689b20b9
Rosco Kalis <roscokalis@gmail.com>
2024-08-26 18:21:15 +0200

    Add Revoke Selected button + some fixes

--- components/common/TableBodyLoader.tsx
+++ components/common/TableBodyLoader.tsx
@@ -1,5 +1,6 @@
 import { Column } from '@tanstack/table-core';
 import { ColumnId } from 'components/allowances/dashboard/columns';
+import { twMerge } from 'tailwind-merge';
 import Loader from './Loader';
 
 interface Props extends React.HTMLAttributes<HTMLTableSectionElement> {
@@ -8,20 +9,19 @@ interface Props extends React.HTMLAttributes<HTMLTableSectionElement> {
 }
 
 const TableBodyLoader = ({ columns, rowCount, ...props }: Props) => {
-  const selectionColumn = columns.findIndex((column) => column.id === ColumnId.SELECT);
-  const adjustedColumnCount = columns.length - (selectionColumn !== -1 ? 1 : 0);
-
   return (
     <tbody {...props}>
       {[...Array(rowCount)].map((_, i) => (
         <tr key={i} className="border-t first:border-0 border-zinc-300 dark:border-zinc-500">
-          {[...Array(adjustedColumnCount)].map((column, j) => (
-            <td key={j} colSpan={j === selectionColumn ? 2 : 1}>
-              <div className="py-2.75 px-2">
-                <Loader isLoading>
-                  <div className="h-7" />
-                </Loader>
-              </div>
+          {columns.map((column, j) => (
+            <td key={j} className={twMerge(column.id === ColumnId.SELECT && 'w-0')}>
+              {column.id === ColumnId.SELECT ? null : (
+                <div className="py-2.75 px-2">
+                  <Loader isLoading>
+                    <div className="h-7" />
+                  </Loader>
+                </div>
+              )}
             </td>
           ))}
         </tr>
