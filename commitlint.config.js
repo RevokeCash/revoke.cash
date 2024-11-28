87517043c501a7b25997f4ca55a40ad736644c4c
Dozie2001 <davidchidozie402@gmail.com>
2024-11-28 06:07:00 +0100

    fix: lint on commitmsg

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,28 @@
-export default { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [2, 'always', [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test'
+      ]],
+      'type-case': [2, 'always', 'lower-case'],
+      'type-empty': [2, 'never'],
+      'scope-case': [2, 'always', 'lower-case'],
+      'subject-case': [
+        2,
+        'never',
+        ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+      ],
+      'subject-empty': [2, 'never'],
+      'subject-full-stop': [2, 'never', '.']
+    }
+  }
