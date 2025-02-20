d30789c4ae3147aac39f4fbc1e199b22719cc113
Rosco Kalis <roscokalis@gmail.com>
2025-02-20 14:59:21 +0100

    fix: fix toast formatting

--- components/common/ToastifyConfig.tsx
+++ components/common/ToastifyConfig.tsx
@@ -8,17 +8,19 @@ const ToastifyConfig = () => (
     toastClassName="flex items-center justify-center text-center border border-black bg-white text-zinc-900 dark:bg-black dark:border-white dark:text-zinc-100"
     progressClassName="bg-black! dark:bg-white!"
     closeButton={({ closeToast, ariaLabel }) => (
-      <button
-        type="button"
-        onClick={(e) => {
-          e.stopPropagation();
-          closeToast(e);
-        }}
-        aria-label={ariaLabel}
-        className="absolute top-0 right-0 m-2 w-6 h-6 text-zinc-500 hover:text-black dark:hover:text-white shrink-0"
-      >
-        <XMarkIcon className="w-6 h-6" />
-      </button>
+      <div className="w-6">
+        <button
+          type="button"
+          onClick={(e) => {
+            e.stopPropagation();
+            closeToast(e);
+          }}
+          aria-label={ariaLabel}
+          className="absolute top-0 right-0 m-2 w-6 h-6 text-zinc-500 hover:text-black dark:hover:text-white shrink-0"
+        >
+          <XMarkIcon className="w-6 h-6" />
+        </button>
+      </div>
     )}
     position="top-right"
     icon={false}
