7506004628fd23aabd6fa0dc41418ce7f8848f81
Rosco Kalis <roscokalis@gmail.com>
2025-04-24 14:20:36 +0200

    fix: increment wallet_sendCalls version

--- lib/hooks/ethereum/useRevokeBatchEip5792.tsx
+++ lib/hooks/ethereum/useRevokeBatchEip5792.tsx
@@ -58,7 +58,7 @@ export const useRevokeBatchEip5792 = (allowances: TokenAllowanceData[], onUpdate
     }
 
     const batchPromise = walletClient.sendCalls({
-      version: '1.0',
+      version: '2.0.0',
       account: walletClient.account!,
       chain: walletClient.chain!,
       calls,
