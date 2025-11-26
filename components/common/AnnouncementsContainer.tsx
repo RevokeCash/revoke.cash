07b51a1c050d2e345f6910ff8929276a3a3146b9
Rosco Kalis <roscokalis@gmail.com>
2025-11-26 10:45:29 -0300

    chore: remove batch fees announcement

--- components/common/AnnouncementsContainer.tsx
+++ components/common/AnnouncementsContainer.tsx
@@ -1,23 +1,7 @@
 'use client';
 
-import AnnouncementBanner from './AnnouncementBanner';
-import Button from './Button';
-
 const AnnouncementsContainer = () => {
-  return (
-    <AnnouncementBanner storageKey="banner-dismissed:2025-10-10">
-      <span>Why we're adding batch revoke fees: </span>
-      <Button
-        style="secondary"
-        size="sm"
-        href="/blog/2025/why-were-adding-batch-revoke-fees"
-        router
-        className="bg-transparent hover:bg-black/20 dark:border-black dark:text-zinc-900"
-      >
-        Learn more
-      </Button>
-    </AnnouncementBanner>
-  );
+  return null;
 };
 
 export default AnnouncementsContainer;
