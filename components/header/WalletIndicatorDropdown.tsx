f8b449bb1b6787098993b4744bacfcbe614fc17d
Rosco Kalis <roscokalis@gmail.com>
2026-03-23 14:58:16 +0100

    fix: no longer redirect on wallet connect by default

--- components/header/WalletIndicatorDropdown.tsx
+++ components/header/WalletIndicatorDropdown.tsx
@@ -4,6 +4,7 @@ import { AUTH_SESSION_QUERY_KEY } from 'lib/auth/session';
 import { useNameLookup } from 'lib/hooks/ethereum/useNameLookup';
 import ky from 'lib/ky';
 import { shortenAddress } from 'lib/utils/formatting';
+import { usePathname } from 'next/navigation';
 import { useTranslations } from 'next-intl';
 import { useConnection, useDisconnect } from 'wagmi';
 import ConnectButton from './ConnectButton';
@@ -16,11 +17,14 @@ interface Props {
 
 const WalletIndicatorDropdown = ({ size, style, className }: Props) => {
   const t = useTranslations();
+  const pathname = usePathname();
 
   const queryClient = useQueryClient();
   const { address: account } = useConnection();
   const { domainName } = useNameLookup(account);
   const { mutate: disconnect } = useDisconnect();
+
+  const isLandingPage = pathname === '/' || /^\/[a-z]{2}$/.test(pathname);
 
   const handleDisconnect = async () => {
     await ky.post('/api/auth/logout').catch(() => {});
@@ -32,16 +36,16 @@ const WalletIndicatorDropdown = ({ size, style, className }: Props) => {
     <div className="flex whitespace-nowrap">
       {account ? (
         <DropdownMenu menuButton={domainName ?? shortenAddress(account, 4)}>
-          <DropdownMenuItem href="/account" router>
-            {t('common.buttons.my_account')}
-          </DropdownMenuItem>
           <DropdownMenuItem href={`/address/${account}`} router retainSearchParams={['chainId']}>
             {t('common.buttons.my_allowances')}
+          </DropdownMenuItem>
+          <DropdownMenuItem href="/account" router>
+            {t('common.buttons.my_account')}
           </DropdownMenuItem>
           <DropdownMenuItem onClick={handleDisconnect}>{t('common.buttons.disconnect')}</DropdownMenuItem>
         </DropdownMenu>
       ) : (
-        <ConnectButton size={size} style={style} className={className} redirect />
+        <ConnectButton size={size} style={style} className={className} redirect={isLandingPage} />
       )}
     </div>
   );
